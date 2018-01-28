d44ef88cf0162d38d130cce4b44fa972351c6959
RunRafter <tcai534@gmail.com>
2018-01-28 00:38:52 -0500

    Changed title from Truffle Box to ShipIt

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
             <OnlyGuestLinks />
             <OnlyAuthLinks />
           </ul>
-          <Link to="/" className="pure-menu-heading pure-menu-link">Truffle Box</Link>
+          <Link to="/" className="pure-menu-heading pure-menu-link">ShipIt!</Link>
         </nav>
 
         {this.props.children}
