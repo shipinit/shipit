2ede6c88758f412498bf50007b36d5f6b3d2da03
Vinit Soni <vinitsoni9869@gmail.com>
2018-01-28 08:33:07 -0500

    remove unnecessary commenting

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -88,14 +88,6 @@ class Dashboard extends Component {
   }
 
   render() {
-    // <main className="container">
-      //   <div className="pure-g">
-      //     <div className="pure-u-1-1">
-      //       <h1>Dashboard</h1>
-      //       <p><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with your own smart contract successfully.</p>
-      //     </div>
-      //   </div>
-      // </main>
 
     return(
       <div>
