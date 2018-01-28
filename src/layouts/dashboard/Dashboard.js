01d44cfa39fb978d42e3ee5457e96229d6166a80
Matthew Black <mblackmblackmblack@gmail.com>
2018-01-28 09:51:02 -0500

    working dashboard

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -3,7 +3,9 @@ import ShippingContract from '../../../build/contracts/Shipping.json'
 // import OpenShipmentsContainer from '../../ui/openshipments/OpenShipmentsContainer'
 import CreateShipmentContainer from '../../ui/createshipment/CreateShipmentContainer'
 import store from '../../store'
-import { Table } from 'reactstrap';
+import { Button, Collapse, Card, CardBody, Container, 
+  Row, Col, InputGroup, InputGroupAddon, 
+  InputGroupText, Input, Table } from 'reactstrap'
 
 const contract = require('truffle-contract')
 
@@ -11,15 +13,116 @@ class Dashboard extends Component {
   constructor(props, { authData }) {
     super(props)
     authData = this.props
-    this.state = {
-      var: '' 
+    this.state = { collapsed: [],
+      arr: [11, 22, 33, 45, 52, 61],
+      stub: {
+        shippingList: [
+          {
+            shippingCost: 14,
+            isTaken: false,
+            licensePlate: "",
+            pickupAddress: "382 Whitney Ave.",
+            deliveryAddress: "56 Victoria St.",
+            enRoute: false,
+            isShipped: true
+          },
+          {
+            shippingCost: 14,
+            isTaken: false,
+            licensePlate: "",
+            pickupAddress: "31 Kind's College Cir.",
+            deliveryAddress: "370 Charles St.",
+            enRoute: false,
+            isShipped: false
+          },
+          {
+            shippingCost: 14,
+            isTaken: false,
+            licensePlate: "",
+            pickupAddress: "45 Richmond St. W.",
+            deliveryAddress: "12 Banff Ave.",
+            enRoute: true,
+            isShipped: false
+          }
+        ]
+      },
+      var: ''
     };
 
+    this.toggle = this.toggle.bind(this);
+    this.getShipmentColour = this.getShipmentColour.bind(this);
+    this.getExpandButton = this.getExpandButton.bind(this);
     this.getShipments = this.getShipments.bind(this);
   }
 
   componentDidMount() {
+    var temp = [];
+    for (var i = 0; i < this.state.stub.shippingList.length; i++) {
+      temp.push(true);
+    }
+    this.setState({ collapsed: temp });
     this.getShipments()
+  }
+
+  toggle(i) {
+    var temp = this.state.collapsed;
+    temp[i] = !temp[i];
+    this.setState({ collapsed: temp });
+  }
+
+  getShipmentColour(i) {
+    var colour = "";
+    if (this.state.stub.shippingList[i].isShipped) { // These are done
+      colour =  "#757575";
+    } else if (this.state.stub.shippingList[i].enRoute) { // These are in progress
+      colour =  "#F4511E";
+    } else { // Can signup with these
+      colour = "#43A047";
+    }
+    return {background: colour};
+    
+  }
+
+  getExpandButton(i) {
+    if (!this.state.stub.shippingList[i].enRoute && !this.state.stub.shippingList[i].isShipped) {
+      return (<Button onClick={(evt) => this.toggle(i)}>{this.state.collapsed[i] ? "signup" : "less"}</Button>);
+    } else if (this.state.stub.shippingList[i].enRoute) {
+      return (<Button onClick={(evt) => this.toggle(i)}>{this.state.collapsed[i] ? "more info" : "less"}</Button>);
+    }
+    return (<div></div>);
+  }
+
+  getExpandedContent(i) {
+    var content = (<div></div>);
+    if (!this.state.stub.shippingList[i].enRoute && !this.state.stub.shippingList[i].isShipped) { // New Transporter can signup
+      content = (
+            <Row>
+            <Col xs="3">
+            <InputGroup>
+              <Input placeholder="shipping cost" type="text" />
+            </InputGroup>
+            </Col>
+            <Col xs="3">
+            <InputGroup>
+              <Input placeholder="license plate" type="text" />
+            </InputGroup>
+            </Col>
+            <Col><Button onClick={(evt) => console.log("TODO")}>confirm</Button></Col>
+            </Row>);
+    } else if (!this.state.stub.shippingList[i].isShipped) { // Show more info, the shipping is in progress
+      content = (
+            <Row>
+            <Col xs="3">
+            Info goes here
+            </Col>
+            <Col xs="3">
+            Other info goes here
+            </Col>
+            <Col>SIGN OFF on Package received by Receiver GOES HERE </Col>
+            </Row>);
+    }
+    return content;
+
   }
 
   getShipments() {
@@ -69,46 +172,36 @@ class Dashboard extends Component {
 
   render() {
     return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <h1>Dashboard</h1>
-            <p><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with your own smart contract successfully.</p>
+      <div>
+        <br/><br/><br/><br/>
+        <Container>
+        <Row style={{ paddingLeft: '20dp'}}>
+          <Col xs="1" className="text-center"><strong>Cost</strong></Col>
+          <Col xs="4" className="text-center"><strong>Pickup Address</strong></Col>
+          <Col xs="4" className="text-center"><strong>Delivery Address</strong></Col>
+        </Row><br/>
+        {this.state.stub.shippingList.map((el, i) => {
+          return (
+            <div key={i}>
+            <Card body inverse style={this.getShipmentColour(i)}><CardBody>
+            <Row>
+              <Col xs="1" className="text-center"><strong>${el.shippingCost}</strong></Col>
+              <Col xs="4" className="text-center">{el.pickupAddress}</Col>
+              <Col xs="4" className="text-center">{el.deliveryAddress}</Col>
+              <Col xs="1" className="text-center"></Col>
 
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colSpan="2">Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-              </tbody>
-            </Table>
-            <CreateShipmentContainer/>
-            {this.state.var}
-          </div>
-        </div>
-      </main>
+              <Col xs="1">{this.getExpandButton(i)}</Col>
+            </Row>
+            <Collapse isOpen={!this.state.collapsed[i]}>
+            {this.getExpandedContent(i)}
+            </Collapse>
+            </CardBody></Card>
+            </div>
+            )
+          })}
+          <CreateShipmentContainer/>
+        </Container>
+      </div>
     )
   }
 }
